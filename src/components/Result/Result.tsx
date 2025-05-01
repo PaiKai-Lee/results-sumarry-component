b7634374a35451496dfe2b8017ea0abcf89ce5f8
Kai Lee <b790618@gmail.com>
2025-05-01 23:34:13 +0800

    fix: fix result card border radius & shadow

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,3 +1,3 @@
 export default function Result({ children }: { children: React.ReactNode }) {
-  return <div className="bg-white md:flex md:w-[720px] md:flex-row">{children}</div>;
+  return <div className="bg-white overflow-hidden shadow-lg md:flex md:w-[720px] md:flex-row md:rounded-3xl">{children}</div>;
 }
